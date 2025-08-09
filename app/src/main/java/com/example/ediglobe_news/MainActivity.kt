e1059fc477ffbeafb74620e5b15b6bc319a5762e
Kumar Atul <mad.final.touch@gmail.com>
2025-08-09 23:09:35 +0530

    news logged

--- app/src/main/java/com/example/ediglobe_news/MainActivity.kt
+++ app/src/main/java/com/example/ediglobe_news/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ediglobe_news
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 //import com.example.ediglobe_news.UI.RecyclerViewAdapter
 import com.example.ediglobe_news.adapter.RecyclerViewAdapter
+import com.example.ediglobe_news.networking.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import com.example.ediglobe_news.data.NewsResponse
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContentView(R.layout.activity_main)
+    val apiKey = "bd96d9a1fc3d4dd9bc392e4580bff69c" // Replace with your NewsAPI.org key
+
+    RetrofitClient.instance.getTopHeadlines("us", apiKey)
+      .enqueue(object : Callback<NewsResponse> {
+        override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
+          if (response.isSuccessful) {
+            val news = response.body()?.articles ?: emptyList()
+            for (article in news) {
+              Log.d("News", "Title: ${article.title}")
+            }
+          }
+        }
+
+        override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+          Log.e("News", "Error: ${t.message}")
+        }
+      })
 
     val dataset =(1..199).map { it.toString() }
     val customAdapter = RecyclerViewAdapter(dataset)
